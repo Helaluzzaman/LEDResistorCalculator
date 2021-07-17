9005eb33d4fcea97523fd93a89dfc2f3e2aad183
Helaluzzaman <64187745+Helaluzzaman@users.noreply.github.com>
2021-07-17 14:02:26 +0600

    get raw result.for different led. Only single mode is working

--- app/src/main/java/com/hbsoft/ledresistorcalculator/fragments/LaunchFragment.kt
+++ app/src/main/java/com/hbsoft/ledresistorcalculator/fragments/LaunchFragment.kt
@@ -1,16 +1,17 @@
 package com.hbsoft.ledresistorcalculator.fragments
 
 import android.os.Bundle
+import android.os.Trace
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.viewModels
 import com.hbsoft.ledresistorcalculator.R
+import com.hbsoft.ledresistorcalculator.data.Led
 import com.hbsoft.ledresistorcalculator.viewModel.LaunchViewModel
 import org.w3c.dom.Text
+import java.lang.Exception
 
 class LaunchFragment : Fragment() {
 
@@ -19,6 +20,11 @@ class LaunchFragment : Fragment() {
     lateinit var etInputVoltage: EditText
     lateinit var bCalculate: Button
     lateinit var tvResult: TextView
+    lateinit var etForwardVoltage: EditText
+    lateinit var etCurrentMax : EditText
+
+
+    lateinit var  currentLed: Led
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,18 +36,51 @@ class LaunchFragment : Fragment() {
         sLedColor = view.findViewById(R.id.s_led_color)
         etInputVoltage = view.findViewById(R.id.et_input_voltage)
         bCalculate = view.findViewById(R.id.b_calculate)
+        etForwardVoltage = view.findViewById(R.id.et_forward_voltage)
+        etCurrentMax = view.findViewById(R.id.et_current_max)
         tvResult = view.findViewById(R.id.tv_result)
         setSpinner()
+        bCalculate.setOnClickListener{
+            var inputVoltage: Double = 0.0
+            val inputVoltageText = etInputVoltage.text.toString()
+            try {
+                inputVoltage = inputVoltageText.toDouble()
+            }catch (exception: Exception){
+                Log.i("input", exception.toString())
+            }
+            val validateInputVoltage = mLaunchViewModel.validator(inputVoltage)
+            if(validateInputVoltage ){
+                mLaunchViewModel.calculateResult(inputVoltage)
+            }else{
+                Toast.makeText(requireContext(), "Input voltage is empty", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        setUiData()
         return view
     }
+
+    private fun setUiData() {
+        mLaunchViewModel.currentLed.observe(viewLifecycleOwner,{
+            currentLed = it
+            etForwardVoltage.setText(it.forwardVoltage_V.toString())
+            etCurrentMax.setText(it.currentMax_mA.toString())
+        })
+        mLaunchViewModel.rawResultOhm.observe(viewLifecycleOwner, {
+            tvResult.setText("Result: " + it.toString() + " ohm (raw result.)")
+        })
+    }
+
     fun setSpinner(){
         val ledNameArray = mLaunchViewModel.LedNameList
         val Adapter = ArrayAdapter(requireContext(),R.layout.support_simple_spinner_dropdown_item,ledNameArray)
         sLedColor.adapter = Adapter
+        sLedColor.onItemSelectedListener = mLaunchViewModel.listener
     }
 
-
-    override fun setHasOptionsMenu(hasMenu: Boolean) {
-        super.setHasOptionsMenu(hasMenu)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.launch_fragment, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
+
 }
--- app/src/main/java/com/hbsoft/ledresistorcalculator/viewModel/LaunchViewModel.kt
+++ app/src/main/java/com/hbsoft/ledresistorcalculator/viewModel/LaunchViewModel.kt
@@ -1,12 +1,49 @@
 package com.hbsoft.ledresistorcalculator.viewModel
 
 import android.app.Application
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.hbsoft.ledresistorcalculator.data.Led
 import com.hbsoft.ledresistorcalculator.data.repository.Repository
+import java.text.FieldPosition
 
 class LaunchViewModel(application: Application): AndroidViewModel(application) {
     val repository: Repository = Repository()
     val LedDataList =repository.getAllLedData()
     val LedNameList = repository.getAllLedNames()
+    var currentLed= MutableLiveData<Led>()
+    var currentMode =  MutableLiveData<String>()
+    var rawResultOhm = MutableLiveData<Double>()
+
+    val listener = object : AdapterView.OnItemSelectedListener{
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            Toast.makeText(application.applicationContext, "${parent!!.getItemAtPosition(position)}, $position", Toast.LENGTH_SHORT).show()
+
+            setLed(position)
+        }
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+    private fun setLed(position: Int) {
+        currentLed.value = LedDataList[position]
+        Log.i("Led", currentLed.value.toString())
+    }
+
+    fun calculateResult(inputVoltage:Double){
+        val fv = currentLed.value!!.forwardVoltage_V
+        val c = currentLed.value!!.currentMax_mA
+        val resultInOhm = ((inputVoltage - fv)/ c)*1000
+        Log.i("result Ohm", resultInOhm.toString())
+        rawResultOhm.value = resultInOhm
+    }
+
+    fun validator(input: Double): Boolean{
+        return input > 0
+    }
 
 }
