3656c68c6d52d8bcaa867039683b397402ae835a
Helaluzzaman <64187745+Helaluzzaman@users.noreply.github.com>
2021-07-23 10:14:20 +0600

    some minor code change done.

--- app/src/main/java/com/hbsoft/ledresistorcalculator/fragments/AboutFragment.kt
+++ app/src/main/java/com/hbsoft/ledresistorcalculator/fragments/AboutFragment.kt
@@ -1,10 +1,11 @@
-package com.hbsoft.ledresistorcalculator
+package com.hbsoft.ledresistorcalculator.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hbsoft.ledresistorcalculator.R
 
 class AboutFragment : Fragment() {
 
